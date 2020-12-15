35b9942fe02cc6b115f62f2fabc9a2c4bbb61cca
Akhila Panackal <akhilacyriacpanackal@gmail.com>
2020-12-15 21:20:12 +0100

    Add comment and Upvote articles

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
+import CommentsList from '../components/CommentsList';
+import Upvotes from '../components/Upvotes';
+import AddComment from '../components/AddComment';
 
 const ArticlePage = ({ match }) => {
+
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
+
+    {/*managing states- npm install whatwg-fetch*/ }
+    const [articleInfo, setArticleInfo] = useState({upvotes:0,comments :[]});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
 
     if (!article) return <NotFoundPage />
 
@@ -13,12 +29,15 @@ const ArticlePage = ({ match }) => {
 
     return (
         <>
-        <h1>{article.title}</h1>
-        {article.content.map((paragraph, key) => (
-            <p key={key}>{paragraph}</p>
-        ))}
-        <h3>Other Articles:</h3>
-        <ArticlesList articles={otherArticles} />
+            <h1>{article.title}</h1>
+            <Upvotes articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+            <CommentsList comments={articleInfo.comments} />
+            <AddComment articleName={name} setArticleInfo={setArticleInfo} />
+            <h3>Other Articles:</h3>
+            <ArticlesList articles={otherArticles} />
         </>
     );
 }
